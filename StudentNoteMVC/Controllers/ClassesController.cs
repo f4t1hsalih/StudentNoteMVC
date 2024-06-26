81ceef1009799eabf501c7c140f891b0512187b1
f4t1hsalih <f4t1hsalih@gmail.com>
2024-06-26 15:31:53 +0300

    Lecture 11 - Dersleri Listeleme

---
+++ StudentNoteMVC/Controllers/ClassesController.cs
@@ -0,0 +1,20 @@
+﻿using StudentNoteMVC.Models.EntityFramework;
+using System.Linq;
+using System.Web.Mvc;
+
+namespace StudentNoteMVC.Controllers
+{
+    public class ClassesController : Controller
+    {
+        // GET: Classes
+        public ActionResult Index()
+        {
+            using (DB_MVCSchoolEntities db = new DB_MVCSchoolEntities())
+            {
+                var classes = db.tbl_classes.ToList();
+                return View(classes);
+            }
+
+        }
+    }
+}
