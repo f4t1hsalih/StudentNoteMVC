517b3ae9a3e0de2d31282828035ad1e2800aff93
f4t1hsalih <f4t1hsalih@gmail.com>
2024-06-26 23:32:47 +0300

    Lecture 14 - Öğrenci Listesi

---
+++ StudentNoteMVC/Controllers/StudentsController.cs
@@ -0,0 +1,19 @@
+﻿using StudentNoteMVC.Models.EntityFramework;
+using System.Linq;
+using System.Web.Mvc;
+
+namespace StudentNoteMVC.Controllers
+{
+    public class StudentsController : Controller
+    {
+        // GET: Students
+        public ActionResult Index()
+        {
+            using (DB_MVCSchoolEntities db = new DB_MVCSchoolEntities())
+            {
+                var students = db.tbl_students.Include("tbl_clubs").ToList();
+                return View(students);
+            }
+        }
+    }
+}
