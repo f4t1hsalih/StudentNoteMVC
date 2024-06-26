65abae38d0418dc514a9ce92c7a63243f6c1237d
f4t1hsalih <f4t1hsalih@gmail.com>
2024-06-26 23:44:42 +0300

    Lecture 15 - Not Listesi

---
+++ StudentNoteMVC/Controllers/GradesController.cs
@@ -0,0 +1,22 @@
+﻿using StudentNoteMVC.Models.EntityFramework;
+using System.Linq;
+using System.Web.Mvc;
+
+namespace StudentNoteMVC.Controllers
+{
+    public class GradesController : Controller
+    {
+        // GET: Grades
+        public ActionResult Index()
+        {
+            using (DB_MVCSchoolEntities db = new DB_MVCSchoolEntities())
+            {
+                var grades = db.tbl_grades
+                               .Include("tbl_students")
+                               .Include("tbl_classes")
+                               .ToList();
+                return View(grades);
+            }
+        }
+    }
+}
