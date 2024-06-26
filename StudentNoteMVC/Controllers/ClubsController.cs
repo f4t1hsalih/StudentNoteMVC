af37e41d3c4c05f86b54f99ded24b1ab0d1c01a7
f4t1hsalih <f4t1hsalih@gmail.com>
2024-06-26 23:08:45 +0300

    Lecture 13 - Kulüp Listesi

---
+++ StudentNoteMVC/Controllers/ClubsController.cs
@@ -0,0 +1,19 @@
+﻿using StudentNoteMVC.Models.EntityFramework;
+using System.Linq;
+using System.Web.Mvc;
+
+namespace StudentNoteMVC.Controllers
+{
+    public class ClubsController : Controller
+    {
+        // GET: Clubs
+        public ActionResult Index()
+        {
+            using (DB_MVCSchoolEntities db = new DB_MVCSchoolEntities())
+            {
+                var clubs = db.tbl_clubs.ToList();
+                return View(clubs);
+            }
+        }
+    }
+}
